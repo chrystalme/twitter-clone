30d0da55f3d251584ab4025d999d35cb76813780
chrystalme <afam_ifeanyi@live.com>
2021-02-23 22:15:14 +0200

    partial complete twitter_clone

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -1,54 +1,60 @@
-# frozen_string_literal: true
-
 class TweeetsController < ApplicationController
-  before_action :set_tweeet, only: %i[show edit update destroy]
+  before_action :set_tweeet, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /tweeets or /tweeets.json
+  # GET /tweeets
+  # GET /tweeets.json
   def index
-    @tweeets = Tweeet.all.order('created_at DESC')
+    @tweeets = Tweeet.all.order("created_at DESC")
     @tweeet = Tweeet.new
   end
 
-  # GET /tweeets/1 or /tweeets/1.json
-  def show; end
+  # GET /tweeets/1
+  # GET /tweeets/1.json
+  def show
+  end
 
   # GET /tweeets/new
   def new
-    @tweeet = Tweeet.new
+    @tweeet = current_user.tweeets.build
   end
 
   # GET /tweeets/1/edit
-  def edit; end
+  def edit
+  end
 
-  # POST /tweeets or /tweeets.json
+  # POST /tweeets
+  # POST /tweeets.json
   def create
-    @tweeet = Tweeet.new(tweeet_params)
+    @tweeet = current_user.tweeets.build(tweeet_params)
 
     respond_to do |format|
       if @tweeet.save
         format.html { redirect_to root_path, notice: 'Tweeet was successfully created.' }
         format.json { render :show, status: :created, location: @tweeet }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @tweeet.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /tweeets/1 or /tweeets/1.json
+  # PATCH/PUT /tweeets/1
+  # PATCH/PUT /tweeets/1.json
   def update
     respond_to do |format|
       if @tweeet.update(tweeet_params)
         format.html { redirect_to @tweeet, notice: 'Tweeet was successfully updated.' }
         format.json { render :show, status: :ok, location: @tweeet }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @tweeet.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /tweeets/1 or /tweeets/1.json
+  # DELETE /tweeets/1
+  # DELETE /tweeets/1.json
   def destroy
     @tweeet.destroy
     respond_to do |format|
@@ -58,14 +64,13 @@ class TweeetsController < ApplicationController
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tweeet
+      @tweeet = Tweeet.find(params[:id])
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_tweeet
-    @tweeet = Tweeet.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def tweeet_params
-    params.require(:tweeet).permit(:tweeet)
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def tweeet_params
+      params.require(:tweeet).permit(:tweeet)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'tweeets#index'
   resources :tweeets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
